9b9991812328940c2c69cd5a7532d2f266ebf717
innercoding <innercoding@gmail.com>
2021-06-09 14:11:28 +0300

    add Kovan

--- truffle-config.js
+++ truffle-config.js
@@ -69,6 +69,17 @@ module.exports = {
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
     },
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(
+          mnemonic,
+          `https://kovan.infura.io/v3/${infura_project_id}`
+        );
+      },
+      network_id: 42,
+      gasPrice: 20000000000,
+      gas: 3716887,
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
